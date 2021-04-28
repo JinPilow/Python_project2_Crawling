904f7645958440eb99b231e83c13c315f7c96833
jmhj456456 <50853896+jmhj456456@users.noreply.github.com>
2021-04-28 11:09:00 +0900

    Update Steam_crawling2.py

--- Python_project2/Steam_crawling2.py
+++ Python_project2/Steam_crawling2.py
@@ -4,10 +4,12 @@ from urllib.request import urlopen
 response = urlopen('https://store.steampowered.com/search/?filter=topsellers')
 soup = BeautifulSoup(response, 'html.parser')
 
+# 게임 이름 가져오기
 # result = soup.select("span.title")
 # for i in result:
 #     print(i.get_text())
 
+# 게임 주소 가져오고 들어가서 카테고리 가져오기
 result = soup.select("#search_resultsRows > a")
 href = []
 for i in result:
