47e157f4cb8cdc9630aaeea1d20b0bbd08c29d53
jmhj456456 <50853896+jmhj456456@users.noreply.github.com>
2021-04-28 11:07:52 +0900

    Update Steam_crawling2.py

--- Python_project2/Steam_crawling2.py
+++ Python_project2/Steam_crawling2.py
@@ -3,7 +3,18 @@ from urllib.request import urlopen
 
 response = urlopen('https://store.steampowered.com/search/?filter=topsellers')
 soup = BeautifulSoup(response, 'html.parser')
-result = soup.select("#search_resultsRows > span.title")
-print(result)
-# for title in result:
-#     print(title.get())
+
+# result = soup.select("span.title")
+# for i in result:
+#     print(i.get_text())
+
+result = soup.select("#search_resultsRows > a")
+href = []
+for i in result:
+    temp = i.attrs['href']
+    print(temp)
+    href.append(temp)
+    response = urlopen(temp)
+    # result2 = soup.select("#game_highlights > div.rightcol > div > div.glance_ctn_responsive_right > div > div.glance_tags.popular_tags > a:nth-child(1)")
+print(href)
+
