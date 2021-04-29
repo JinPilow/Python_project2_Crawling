53edffb5f6c942e84ebbe6ac7a8267821ef1c36e
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-04-29 14:40:04 +0900

    selenium

--- Python_project2/Steam_crawling.py
+++ Python_project2/Steam_crawling.py
@@ -1,55 +1,27 @@
-from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 from bs4 import BeautifulSoup
-'''
-html = urlopen("https://store.steampowered.com/search/?filter=topsellers")
 
-soup = BeautifulSoup(html, 'html.parser')
-title = soup.find_all(class_='title')
-for i in title:
-    print(i.text.strip())
-'''
-'''
-import selenium
-from selenium import webdriver
-from selenium.webdriver import ActionChains
+driver = webdriver.Chrome()
+url = "https://store.steampowered.com/search/?filter=topsellers"
+driver.get(url)
 
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+dic = dict()
+for i in range(1,6):
+    link = driver.find_element_by_css_selector('#search_resultsRows > a:nth-child(' + str(i) + ')')
+    link.click()
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('div', class_='apphub_AppName')
+    tag = soup.find_all('a', class_='app_tag')
+    title = title.get_text().strip()
+    li = []
+    for i in tag[:3]:
+        li.append(i.get_text().strip())
+    dic.setdefault(title, li)
+    driver.back()
+print(dic)
 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-
-URL = 'https://store.steampowered.com/search/?filter=topsellers'
-
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get(url=URL)
-print(driver.current_url)
-driver.close()
-driver.implicitly_wait(time_to_wait=5)
-'''
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep
-
-options = webdriver.ChromeOptions()
-options.add_argument('window-size=1920,1080')
-
-driver = webdriver.Chrome('chromedriver', options=options)
-driver.implicitly_wait(5)
-
-driver.get(url='https://www.google.com/')
-
-search_box = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-
-search_box.send_keys('greeksharifa.github.io')
-search_box.send_keys(Keys.RETURN)
-
-elements = driver.find_elements_by_xpath('//*[@id="rso"]/div[*]/div/div[1]/a/h3/span')
-
-for element in elements:
-    print(element.text)
-    print(element.text, file=open('gorio.txt', 'w', encoding='utf-8'))
-
-sleep(3)
-driver.close()
+#time.sleep(3)
+driver.quit()
