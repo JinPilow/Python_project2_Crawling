8e267f3fc79247df67f0f4e7c5b4649054c51296
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-10 13:21:34 +0900

    selenium

--- Python_project2/Steam_crawling.py
+++ Python_project2/Steam_crawling.py
@@ -9,29 +9,40 @@ driver.get(url)
 
 dic = dict()
 i = 1
-while i <= 100:
+while i <= 2:
+    link = driver.find_element_by_css_selector('#search_resultsRows > a:nth-child(' + str(i) + ')')
+    link.click()
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
     try:
-        link = driver.find_element_by_css_selector('#search_resultsRows > a:nth-child(' + str(i) + ')')
-        link.click()
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        title = soup.find('div', class_='apphub_AppName')
+        if str(soup.select_one('.apphub_AppName')) == "None":
+            title = soup.select_one('.pageheader')
+        else:
+            title = soup.select_one('.apphub_AppName')
         tag = soup.find_all('a', class_='app_tag')
-        title = title.get_text().strip()
+        print(title.get_text())
         li = []
         for category in tag[:3]:
             li.append(category.get_text().strip())
         dic.setdefault(title, li)
-        driver.back()
     except:
         #if 'https://store.steampowered.com/agecheck/app/' in html:
-        print("여기")
         link = driver.find_element_by_css_selector('#ageYear > option:nth-child(96)')
         link.click()
         link = driver.find_element_by_css_selector('#app_agegate > div.main_content_ctn > div.agegate_text_container.btns > a:nth-child(1)')
         link.click()
+        if str(soup.select_one('.apphub_AppName')) == "None":
+            title = soup.select_one('.pageheader')
+        else:
+            title = soup.select_one('.apphub_AppName')
+        tag = soup.find_all('a', class_='app_tag')
+        li = []
+        for category in tag[:3]:
+            li.append(category.get_text().strip())
+        dic.setdefault(title, li)
     finally:
         i += 1
+        driver.back()
 print(dic)
 
 #time.sleep(3)
