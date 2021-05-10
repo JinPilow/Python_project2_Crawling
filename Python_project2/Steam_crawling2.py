5f3d390e248f1866788f88dac170d2c0304ba733
jmhj456456 <50853896+jmhj456456@users.noreply.github.com>
2021-05-10 12:48:54 +0900

    태그 통계 성공
    각 태그 3개씩 가져오고 각 개수 출력까지 성공

--- Python_project2/Steam_crawling2.py
+++ Python_project2/Steam_crawling2.py
@@ -3,36 +3,36 @@ from urllib.request import urlopen
 import time
 from multiprocessing import Pool, Manager
 
-count = 0
-tags = {}
-
 
 def get_href():
+    # 클릭 대상의 a태그 옵션들을 가져온다
     hrefs = soup.select("#search_resultsRows > a")
     data = []
     for href in hrefs:
+        # href 내용물을 가져온다
         data.append(href.attrs['href'])
     return data
 
 
 # 게임 주소 가져오고 들어가서 카테고리 가져오기
 def get_content(href):
-    global count
-    global tags
+    cont = {}
+
     response = urlopen(href)
     soup = BeautifulSoup(response, 'html.parser')
+
+    # 태그들을 가져온다 (3개만)
     result2 = soup.select("a.app_tag")
     i = 0
-    for j in result2:
-        if j.get_text(" ", strip=True) in tags:
-            tags[j.get_text(" ", strip=True)] = tags[j.get_text(" ", strip=True)] + 1
-        else:
-            tags[j.get_text(" ", strip=True)] = 1
+    for content in result2:
+        cont[content.get_text(" ", strip=True)] = 1
         i = i + 1
         if i == 3:
             break
-    count = count + 1
+
     print("processing...")
+
+    return cont
 
 
 if __name__ == '__main__':
@@ -41,16 +41,30 @@ if __name__ == '__main__':
     response = urlopen('https://store.steampowered.com/search/?sort_by=_ASC&os=win&filter=globaltopsellers')
     soup = BeautifulSoup(response, 'html.parser')
 
-    title = []
+    # 각 게임 태그 가져오기 (멀티프로세싱으로 속도 개선)
+    pool = Pool(processes=16)
+    tlist = pool.map(get_content, get_href())
 
     # 게임 이름 가져오기
     result = soup.select("span.title")
+    j = 0
     for i in result:
-        title.append(i.get_text())
+        tlist[j]["title"] = i.get_text()
+        j = j + 1
 
-    pool = Pool(processes=16)
-    pool.map(get_content, get_href())
+    print("크롤링 갯수 :{}".format(len(result)))
 
-    print(count)
-    print(tags)
+    # 태그 개수 통계
+    total_tag = {}
+    for i in tlist:
+        for tag in list(i.keys()):
+            if tag in total_tag:
+                total_tag[tag] = total_tag[tag] + 1
+            else:
+                total_tag[tag] = 1
+
+    # 제목 갯수는 삭제
+    del total_tag['title']
+    print(total_tag)
+
     print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
