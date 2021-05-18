a513ba853388ed52f0f321f9ddb12baf11bfaead
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-05-18 13:27:35 +0900

    genre

--- Python_project2/Steam_crawling.py
+++ Python_project2/Steam_crawling.py
@@ -78,6 +78,7 @@ driver.quit()
 '''
 
 from bs4 import BeautifulSoup
+import multiprocessing
 import requests
 import re
 import matplotlib.pyplot as plt
@@ -87,70 +88,60 @@ import pandas as pd
 from pandas import DataFrame
 
 
+
 html = requests.get('https://store.steampowered.com/search/?filter=topsellers')
 soup = BeautifulSoup(html.content, 'html.parser')
 
 title = soup.select('span.title')
 price = soup.select('div.col.search_price')
-tag_site = soup.find_all('a', href = re.compile('https://store.steampowered.com/'+\
+genre_site = soup.find_all('a', href = re.compile('https://store.steampowered.com/'+\
                                                 'app/|bundle/|sub/'))
-
-# def get_tag(href):
-#     html = requests.get(href)
-#     soup = BeautifulSoup(html.content, 'html.parser')
-#     tag = soup.find_all('a', href= re.compile('https://store.steampowered.com/' + 'tags/|genre/'))
-#     li = []
-#     for category in tag:
-#         li.append(category.get_text().strip())
-#     return li
-
-def get_tag(href):
+# url에 접속해 장르를 가져오는 함수
+def get_genre(href):
     html = requests.get(href)
     soup = BeautifulSoup(html.content, 'html.parser')
-    tag = soup.find_all('a', href = re.compile('https://store.steampowered.com/genre/'))
-    li = []
-    for category in tag:
-        li.append(category.get_text().strip())
-    return li
+    tags = soup.find_all('a', href= re.compile('https://store.steampowered.com/' + 'genre/'))
+    genre = []
+    for category in tags[2:]:
+        genre.append(category.get_text().strip())
+    return genre
 
-for i in tag_site:
+# 가격에서 콤마를 없애고 정수형으로 만드는 함수
+def comma_to_int(string):
+    number = re.sub(",", "", string)
+    return int(number)
+
+list_title = [i.get_text() for i in title]
+list_genre = []
+list_price = []
+
+# 리스트에 장르 저장
+for i in genre_site:
     href = i.attrs['href']
-    print(get_tag(href))
+    list_genre.append(get_genre(href))
 
+# 리스트에 가격 저장
+for i in price:
+    if i.get_text().strip().count("₩") == 1:
+        a = comma_to_int(i.get_text().strip().replace("₩", "").strip())
+        list_price.append(a)
+    else:
+        s = i.get_text().strip().split("₩")
+        a = comma_to_int(s[1].strip())
+        list_price.append(a)
 
-# for i in tag_site:
-#     href = i.attrs['href']
-#     print(href)
-
-# def comma_to_int(string):
-#     number = re.sub(",", "", string)
-#     return int(number)
-#
-# list_title = [i.get_text() for i in title]
-# list_tag = []
-# list_price = []
-#
-#
-#
-# for i in price:
-#     if i.get_text().strip().count("₩") == 1:
-#         a = comma_to_int(i.get_text().strip().replace("₩", "").strip())
-#         list_price.append(a)
-#     else:
-#         s = i.get_text().strip().split("₩")
-#         a = comma_to_int(s[1].strip())
-#         list_price.append(a)
-# #
 # title_price = dict(zip(list_title, list_price))
 
+# 시각화
+sns.barplot(x=list_title,y=list_price)
+plt.xticks(rotation = 90, fontsize = 6)
+plt.show()
 
-# sns.barplot(x=list_title,y=list_price)
-# plt.xticks(rotation = 90, fontsize = 6)
-# plt.show()
-#
-# df = DataFrame({'col_1': list_title, 'col_2': list_price, 'col_3': list_tag})
-# print(df)
+# 데이터프레임화
+# df = DataFrame({'col_1': list_title, 'col_2': list_price})
+df = DataFrame(list_title, list_price, list_genre)
+print(df)
 
-# factor_price = pd.cut(df.col_2, 4)
-# group_price = df.col_2.groupby(factor_price)
-# print(group_price.agg(['count', 'mean', 'std', 'min', 'max']))
+factor_price = pd.cut(df.col_2, 4)
+group_price = df.col_2.groupby(factor_price)
+print(group_price.agg(['count', 'mean', 'std', 'min', 'max']))
